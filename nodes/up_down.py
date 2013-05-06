edfa93e9d81b82fa7dd23682edf4bdaf480471f4
Sameer Parekh <sameer@falkorsystems.com>
2013-05-06 10:11:04 -0400

    change to regular scanning mode

--- nodes/up_down.py
+++ nodes/up_down.py
@@ -11,19 +11,19 @@ rospy.init_node( 'up_down' )
 tilt_pub = rospy.Publisher( "tilt_controller/command", Float64 )
 
 current_tilt = 0.0
-
+max_tilt = np.pi/4
+tilt_step = np.pi/32
+center_pause = 1
+step_pause = 0.3
+direction = 1
 
 while True:
+    if current_tilt == 0.0:
+        rospy.sleep( center_pause )
+    else:
+        rospy.sleep( step_pause )
+    
+    current_tilt += direction * tilt_step
+    if abs( current_tilt ) >= max_tilt:
+        direction = -direction
     tilt_pub.publish( Float64( current_tilt ) )
-    random_wait = random.gauss( 4, 2 )
-    rospy.sleep(random_wait)
-
-    tilt_change = random.gauss( 0, np.pi/2 )
-    if current_tilt == -np.pi/4:
-        tilt_change = abs( tilt_change )
-    if current_tilt == np.pi/4:
-        tilt_change = - abs( tilt_change )
-
-    current_tilt += tilt_change
-    current_tilt = min( max( current_tilt, -np.pi/4 ), np.pi/4 )
-
